f5aea389e0ec054ba093a46e2c7a1ab21d5b14e7
kkafi09 <afi.firda@gmail.com>
2022-11-01 19:11:06 +0700

    add: update directory

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "prettier",
+    "plugin:prettier/recommended",
+    "plugin:@next/next/recommended",
+  ],
+  plugins: ["prettier"],
+  rules: {},
+  settings: {},
+  overrides: [],
+};
